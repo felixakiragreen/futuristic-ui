7c876fb13eb107bf937bd1910b515fe7145046fa
Felix Green <work@felix.green>
2021-01-24 12:31:00 -0500

    init project + assets

---
+++ FutureUI/Shared/FutureUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureUIApp.swift
+//  Shared
+//
+//  Created by Felix Akira Green on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct FutureUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
