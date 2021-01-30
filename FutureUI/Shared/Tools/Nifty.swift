36d957bdc35d165af8d29a9fe12f6ddb438f0e9d
Felix Green <work@felix.green>
2021-01-29 23:30:01 -0500

    add get & set for collections

--- FutureUI/Shared/Tools/Nifty.swift
+++ FutureUI/Shared/Tools/Nifty.swift
@@ -13,8 +13,47 @@ func clamp<T: Comparable>(value: T, lower: T, upper: T) -> T {
 }
 
 extension Collection {
-	/// This allows for indexes to be fetched safely
+	/// This allows for ANY index to be fetched & set safely
 	subscript(optional i: Index) -> Iterator.Element? {
-		return self.indices.contains(i) ? self[i] : nil
+		get {
+			return self.indices.contains(i) ? self[i] : nil
+		}
+		set(newValue) {
+			if self.indices.contains(i) {
+				self[optional: i] = newValue
+			}
+		}
+	}
+	
+	/// This allows for an OPTIONAL index to be fetched & set safely
+	subscript(optional i: Index?) -> Iterator.Element? {
+		get {
+			if let index = i {
+				return self[optional: index]
+			}
+			return nil
+		}
+		set(newValue) {
+			if let index = i {
+				self[optional: index] = newValue
+			}
+		}
+	}
+}
+
+/// Allows for an optional key to be used for a DIctionary
+extension Dictionary {
+	subscript(optional k: Key?) -> Value? {
+		get {
+			if let key = k {
+				return self[key]
+			}
+			return nil
+		}
+		set(newValue) {
+			if let key = k {
+				self[key] = newValue
+			}
+		}
 	}
 }
