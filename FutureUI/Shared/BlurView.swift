be126ce6a99e79027af3446209a912292851c5e0
Felix Green <work@felix.green>
2021-02-04 12:29:26 -0500

    start adding BlurView stuff

---
+++ FutureUI/Shared/BlurView.swift
@@ -0,0 +1,177 @@
+//
+//  BlurView.swift
+//  FutureUI
+//
+//  Created by Felix Akira Green on 2/4/21.
+//
+
+import SwiftUI
+
+
+
+// MARK: - PREVIEW
+struct BlurView_Previews: PreviewProvider {
+	static var previews: some View {
+		BlurView()
+	}
+//	static var previews: some View {
+//		ZStack {
+//			LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//
+//			VStack {
+//				VisualEffectBlur(material: .contentBackground)
+//					.frame(height: 40)
+//				//					.padding()
+//				VisualEffectBlur(material: .fullScreenUI)
+//					.frame(height: 40)
+//				//					.padding()
+//				VisualEffectBlur(material: .titlebar)
+//					.frame(height: 40)
+//
+//				VisualEffectBlur(material: .headerView)
+//					.frame(height: 40)
+//
+//				VisualEffectBlur(material: .windowBackground)
+//					.frame(height: 40)
+//
+//				VisualEffectBlur(material: .sidebar)
+//					.frame(height: 40)
+//			}
+//			.padding()
+//
+//			Text("Hello World!")
+//		}
+//		//		  .frame(width: 200, height: 100)
+//		.previewLayout(.sizeThatFits)
+//	}
+}
+
+
+struct BlurView: View {
+	
+	// MARK: - PROPS
+	
+	let materials: [NSVisualEffectView.Material] = [
+		.titlebar,
+		.selection,
+		.menu,
+		.popover,
+		.sidebar,
+		.headerView,
+		.sheet,
+		.windowBackground,
+		.hudWindow, // ooh
+		.fullScreenUI,
+		.toolTip,
+		.contentBackground,
+		.underWindowBackground,
+		.underPageBackground
+	]
+	let materialNames: [String] = [
+		".titlebar",
+		".selection",
+		".menu",
+		".popover",
+		".sidebar",
+		".headerView",
+		".sheet",
+		".windowBackground",
+		".hudWindow",
+		".fullScreenUI",
+		".toolTip",
+		".contentBackground",
+		".underWindowBackground",
+		".underPageBackground"
+	]
+	
+	@State var material: Int = 8
+	
+	// MARK: - BODY
+	var body: some View {
+		let blurMaterialName = materialNames[optional: material]
+		
+		return ZStack {
+			if let blurMaterial = materials[optional: material] {
+				VisualEffectBlur(material: blurMaterial)
+			}
+		VStack {
+//		Text("currentMaterial: \(blurMaterialName)")
+		}
+		.onTapGesture {
+			if material < materials.count {
+				material += 1
+			} else {
+				material = 0
+			}
+//			print("selecting:", materialNames[optional: material])
+		}
+		.frame(width: 300, height: 300)
+		}
+	}
+}
+
+#if os(macOS)
+
+// MARK: - VisualEffectBlur
+
+/**
+TODO:
+- [ ] add state
+- [ ] add blendMode
+https://developer.apple.com/documentation/appkit/nsvisualeffectview
+*/
+
+struct VisualEffectBlur: View {
+	var material: NSVisualEffectView.Material
+	// state
+	// blendMode
+	
+	init(material: NSVisualEffectView.Material = .headerView) {
+		self.material = material
+	}
+	
+	var body: some View {
+		Representable(material: material)
+			.accessibility(hidden: true)
+	}
+}
+
+// MARK: - Representable
+
+extension VisualEffectBlur {
+	struct Representable: NSViewRepresentable {
+		var material: NSVisualEffectView.Material
+		
+		func makeNSView(context: Context) -> NSVisualEffectView {
+			context.coordinator.visualEffectView
+		}
+		
+		func updateNSView(_ view: NSVisualEffectView, context: Context) {
+			context.coordinator.update(material: material)
+		}
+		
+		func makeCoordinator() -> Coordinator {
+			Coordinator()
+		}
+	}
+	
+	class Coordinator {
+		let visualEffectView = NSVisualEffectView()
+		
+		init() {
+			visualEffectView.blendingMode = .withinWindow
+			visualEffectView.state = .active
+		}
+		
+		func update(material: NSVisualEffectView.Material) {
+			visualEffectView.material = material
+		}
+	}
+}
+
+
+#elseif os(iOS)
+
+
+
+#endif
---
+++ FutureUI/macOS/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  FutureUI (macOS)
+//
+//  Created by Felix Akira Green on 2/4/21.
+//
+
+import Cocoa
+import SwiftUI
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+	var window: NSWindow!
+	
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+		
+		let contentView = ContentView()
+			.edgesIgnoringSafeArea(.top) // to extend entire content under titlebar
+		
+		// Create the window and set the content view.
+		window = NSWindow(
+			contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+			styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+			backing: .buffered, defer: false)
+		
+		window.center()
+		window.setFrameAutosaveName("Main Window")
+		
+		window.titlebarAppearsTransparent = true
+		window.styleMask.insert(.fullSizeContentView)
+		
+		window.backgroundColor = NSColor(Color.clear)
+		window.isMovableByWindowBackground = true
+		
+		window.contentView = NSHostingView(rootView: contentView)
+		window.makeKeyAndOrderFront(nil)
+	}
+	
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+	
+}
